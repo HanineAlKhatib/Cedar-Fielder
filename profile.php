24e3b4fd9681c3710acc8e1454c702691699cf36
mdhaini2 <mohamad.dhaini02@lau.edu>
2023-05-12 02:57:25 +0300

    'debug'

--- profile.php
+++ profile.php
@@ -11,40 +11,41 @@
 
   <link rel="stylesheet" href="./css/profile.css" />
 </head>
-<?php require_once('header.php') 
+<?php require_once('header.php')
 
 
 ?>
 
- 
-  <main class="container mt-5">
-    <section id="user-profile" class="row">
-      <div id="user-info" class="col-md-8">
-        <h2 id="username"><?=$username?></h2>
-        <p id="phone-number"><?=$phoneNumber?></p>
-        <button id="edit-credentials" class="btn btn-primary">
-          <i class="fas fa-edit"></i> Edit Credentials
-        </button>
-        <button id="post-field" class="btn btn-success" onclick="window.location.href='../poster/post.html';">
-          <i class="fas fa-plus"></i> Post a Field
-        </button>
-      </div>
-    </section>
-    <section id="user-fields" class="mt-5">
-      <h2>My Posted Fields</h2>
-      <div class="field-list" id="my-posted-fields"></div>
-      <h2 class="mt-4">Saved Fields</h2>
-      <div class="field-list" id="saved-fields"></div>
-      <h2 class="mt-4">Rented Fields</h2>
-      <div class="field-list" id="rented-fields"></div>
-    </section>
-  </main>
-  
-  <footer class="text-center py-3">
-    <p>&copy; 2023 Sports Field Rentals. All rights reserved.</p>
-  </footer>
-  <!--SCRIPT-->
-  <script src="./js/profile.js"></script>
+
+<main class="container mt-5" style="
+    margin-top: 8rem !important;">
+  <section id="user-profile" class="row">
+    <div id="user-info" class="col-md-8">
+      <h2 id="username"><?= $username ?></h2>
+      <p id="phone-number"><?= $phoneNumber ?></p>
+      <button id="edit-credentials" class="btn btn-primary">
+        <i class="fas fa-edit"></i> Edit Credentials
+      </button>
+      <button id="post-field" class="btn btn-success" onclick="window.location.href='../poster/post.html';">
+        <i class="fas fa-plus"></i> Post a Field
+      </button>
+    </div>
+  </section>
+  <section id="user-fields" class="mt-5">
+    <h2>My Posted Fields</h2>
+    <div class="field-list" id="my-posted-fields"></div>
+    <h2 class="mt-4">Saved Fields</h2>
+    <div class="field-list" id="saved-fields"></div>
+    <h2 class="mt-4">Rented Fields</h2>
+    <div class="field-list" id="rented-fields"></div>
+  </section>
+</main>
+
+<footer class="text-center py-3">
+  <p>&copy; 2023 Sports Field Rentals. All rights reserved.</p>
+</footer>
+<!--SCRIPT-->
+<script src="./js/profile.js"></script>
 
 </body>
 
