05bff676b9ae598428ac28b6d0f0012b17d62dd8
babybleep <alkhatibhanine@gmail.com>
2023-05-12 02:55:44 +0300

    fixed poster

--- field.php
+++ field.php
@@ -1,108 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    />
 
-    <link rel="stylesheet" href="./css/field.css" />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
 
-    <title>Field Detail</title>
-  </head>
-  <body>
-    <!-- NAVBAR -->
-    <div id="navbar-placeholder"></div>
+  <link rel="stylesheet" href="./css/field.css" />
+
+  <title>Field Detail</title>
+</head>
+
+<body>
+  <!-- NAVBAR -->
+  <div id="navbar-placeholder"></div>
 
 
-    <!-- FIELD DESCRIPTION -->
+  <!-- FIELD DESCRIPTION -->
 
-    <div class="container field-description">
-      <div class="row">
-        <div class="col-md-12">
-          <img
-            src="https://via.placeholder.com/600x400"
-            alt="Field Image"
-            class="field-image"
-          />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8">
-          <h1 class="field-name">Field Name</h1>
-          <div class="field-details">
-            <div class="field-detail">
-              <strong>Size:</strong> 100 x 50 yards
-            </div>
-            <div class="field-detail"><strong>Sport Type:</strong> Soccer</div>
-            <div class="field-detail">
-              <strong>Location:</strong> Cedar Park
-            </div>
-            <div class="field-detail">
-              <strong>Indoor/Outdoor:</strong> Outdoor
-            </div>
-            <div class="field-detail"><strong>Status:</strong> Available</div>
-            <div class="field-detail">
-              <strong>Description:</strong> Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Nunc scelerisque neque eu leo
-              lobortis, eu pellentesque augue vestibulum.
-            </div>
-            <div class="field-detail">
-              <strong>Days Open for Rent:</strong> Monday - Friday
-            </div>
-            <div class="field-detail">
-              <strong>Price:</strong> $100 per hour
-            </div>
-            <div class="field-detail">
-              <strong>Rent Period:</strong> 2 hours minimum
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <form id="rent-form" class="form">
-            <h2 class="form-title">Book this Field</h2>
-            <div class="form-group">
-              <label for="rent-date" class="form-label">Choose a Date:</label>
-              <input
-                type="date"
-                class="form-control"
-                id="rent-date"
-                name="rent-date"
-                required
-              />
-            </div>
-            <div class="form-group">
-              <label for="rent-time" class="form-label">Choose a Time:</label>
-              <input
-                type="time"
-                class="form-control"
-                id="rent-time"
-                name="rent-time"
-                required
-              />
-            </div>
-            <div class="form-group text-center">
-              <button class="rent-btn" type="submit">Request Booking</button>
-            </div>
-          </form>
-        </div>
+  <div class="container field-description">
+    <div class="row">
+      <div class="col-md-12">
+        <img src="https://via.placeholder.com/600x400" alt="Field Image" class="field-image" />
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-8">
+        <h1 class="field-name">Field Name</h1>
+        <div class="field-details">
+          <div class="field-detail">
+            <strong>Size:</strong> 100 x 50 yards
+          </div>
+          <div class="field-detail"><strong>Sport Type:</strong> Soccer</div>
+          <div class="field-detail">
+            <strong>Location:</strong> Cedar Park
+          </div>
+          <div class="field-detail">
+            <strong>Indoor/Outdoor:</strong> Outdoor
+          </div>
+          <div class="field-detail">
+            <strong>Description:</strong> Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc scelerisque neque eu leo
+            lobortis, eu pellentesque augue vestibulum.
+          </div>
+          <div class="field-detail">
+            <strong>Days Open for Rent:</strong> Monday - Friday
+          </div>
+          <div class="field-detail">
+            <strong>Price:</strong> $100 per hour
+          </div>
+          <div class="field-detail">
+            <strong>Rent Period:</strong> 2 hours minimum
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <form id="rent-form" class="form">
+          <h2 class="form-title">Book this Field</h2>
+          <div class="form-group">
+            <label for="rent-date" class="form-label">Choose a Date:</label>
+            <input type="date" class="form-control" id="rent-date" name="rent-date" required />
+          </div>
+          <div class="form-group">
+            <label for="rent-time" class="form-label">Choose a Time:</label>
+            <input type="time" class="form-control" id="rent-time" name="rent-time" required />
+          </div>
+          <div class="form-group text-center">
+            <button class="rent-btn" type="submit">Request Booking</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 
-    <footer>
-      <p>&copy; 2023 Sports Field Rentals. All rights reserved.</p>
-    </footer>
+  <footer>
+    <p>&copy; 2023 Sports Field Rentals. All rights reserved.</p>
+  </footer>
 
-    <!-- SCRIPT -->
-    
-    <script src="./js/field.js"></script>
-  </body>
+  <!-- SCRIPT -->
+
+  <script src="./js/field.js"></script>
+</body>
+
 </html>
