c5efa0e58c855721a7b58cd17e8a6b9f14d4abe2
mdhaini2 <mohamad.dhaini02@lau.edu>
2023-05-12 02:03:25 +0300

    'debug'

--- browse.php
+++ browse.php
@@ -11,7 +11,12 @@ $searchedFields = $table->findSql($query);
 <body class="homepage">
     <?php require_once('slider.php')?>
 <main>
-<h1>Welcome to Sports Field Rentals!</h1>
+    <h1>Browse</h1>
+<form id="search-form" action="browse.php" method="GET">
+      <input type="text" id="search-input" name="search" placeholder="Search by field name or location" />
+      <button type="submit">Search</button>
+    </form>
+
     <section id="lately-posted">
       <h3>Browse</h3>
       <div class="field-list" id="lately-posted-fields">
