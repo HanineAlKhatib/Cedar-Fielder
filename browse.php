6e33248d2a10b80bbefaafbf30b5ade0f129976d
mdhaini2 <mohamad.dhaini02@lau.edu>
2023-05-12 01:10:49 +0300

    'debug'

--- browse.php
+++ browse.php
@@ -1,7 +1,10 @@
 <?php
 require_once('header/header.php');
-$search = $_GET['search'];
 
-$searchedFields = $table->findSql("SELECT distinct(*) FROM fields WHERE name LIKE '%$search%' OR address LIKE '%$search%'");
+$search = $_GET['search']; // Assuming you're retrieving the search query from the GET parameter
+
+$query = "SELECT * FROM fields WHERE name LIKE '%$search%' OR address LIKE '%$search%'";
+$searchedFields = $table->findSql($query);
+
 require_once('../Slider/slider.php')
 ?>
