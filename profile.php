3d8b5b2e2edc0aaf1e24a099210875fd7e5f5541
babybleep <alkhatibhanine@gmail.com>
2023-05-12 09:15:48 +0300

    debug

--- profile.php
+++ profile.php
@@ -39,7 +39,7 @@ $myRentedFields = $table->findSql("SELECT fields.*, rent_fields.created_at  FROM
     <div class="field-list" id="most-rented-fields">
       <?php foreach ($mypostedField as $field) : ?>
         <div>
-          <a href="field.php?id=<?= $field['id']; ?>" class="field">
+          <a href="field.php?id=<?= $field['id']; ?>" class="field" style='color:black; text-decoration: none;'>
 
             <img src="<?= $field['image_url']; ?>" alt="Field Image" />
 
