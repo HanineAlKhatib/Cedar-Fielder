6e4fe06f5459d76f5bf67bb1ad098ffcb85d3f8e
mdhaini2 <mohamad.dhaini02@lau.edu>
2023-05-12 06:32:42 +0300

    'debug'

--- js/field.js
+++ js/field.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
     $('#rent-form').submit(function(event) {
       event.preventDefault(); // Prevent the form from submitting normally
-      
+  
       // Get the min_rent_hours from the data attribute
       const minRentHours = $(this).data('min-rent-hours');
-      const fieldId = $(this).data('field-id');
-      
+      const fieldId = $(this).attr('field-id');
+  
       // Get the form data
       var formData = $(this).serialize();
-      
-      // Append the min_rent_hours to the form data
+  
+      // Append the min_rent_hours and field_id to the form data
       formData += '&min-rent-hours=' + minRentHours;
       formData += '&field_id=' + fieldId;
-      console.log(minRentHours);
+    console.log(fieldId);
       // Make an AJAX request
       $.ajax({
         type: 'POST',
-        url: '/cedar-fielder/Backend/reservation.php', 
+        url: '/cedar-fielder/Backend/reservation.php',
         data: formData,
         success: function(response) {
-      
           if (response.success) {
-     
             alert(response.message);
           } else {
-
             alert(response.message);
           }
         },
