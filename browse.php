e6c86cfb02748b8863fb7a793f4352e1d3e2f23a
mdhaini2 <mohamad.dhaini02@lau.edu>
2023-05-12 04:49:29 +0300

    'debug;

--- browse.php
+++ browse.php
@@ -1,10 +1,7 @@
 <?php
 require_once('header.php');
 
-$search = $_GET['search']; // Assuming you're retrieving the search query from the GET parameter
 
-$query = "SELECT * FROM fields WHERE name LIKE '%$search%' OR address LIKE '%$search%'";
-$searchedFields = $table->findSql($query);
 ?>
   <link rel="stylesheet" href="./css/index.css" />
 <body class="homepage">
